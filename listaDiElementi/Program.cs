fea878ff6c851467f195e7c87d95326828ec3c3f
Diego <56691356+sians28@users.noreply.github.com>
2024-02-17 17:51:55 +0100

    Add files via upload

---
+++ listaDiElementi/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<string> listaDiAnimali = new List<string>() { "Gatto", "Cavallo", "Cane", "Pappagallo", "Serpente" };
+
+        foreach (string valore in listaDiAnimali)
+        {
+            Console.WriteLine(valore);
+        }
+
+        
+
+        string sommaListaDiAnimali = Program.GetSommaStringheLista(listaDiAnimali);
+        Console.WriteLine(sommaListaDiAnimali);
+
+
+
+    }
+
+    internal static string GetSommaStringheLista(List<string> lista){
+        string variabile= "";
+        int i =0;
+        foreach(string elemento in lista){
+            
+            variabile += lista[i];
+            i++;
+
+        }
+        return variabile;
+    }
+
+}
