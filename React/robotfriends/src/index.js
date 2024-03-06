db91be73262d2e9bb8b20e761903f54637231ce5
Diego Nuzzolo <nuzzolo1000@gmail.com>
2024-03-06 01:35:16 +0100

    last update

--- React/robotfriends/src/index.js
+++ React/robotfriends/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {createRoot} from 'react-dom/client'
-import App from './App'
+import { createRoot } from 'react-dom/client'
+import App from './container/App'
 import './index.css';
 import "tachyons";
 
 
-const container= document.getElementById('root')
+const container = document.getElementById('root')
 const root = createRoot(container)
 root.render(<App />);
