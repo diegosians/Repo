ec34783fe76e71205ac1ea4f8041f9d25058e569
diegosians <nuzzolo1000@gmail.com>
2024-03-12 19:36:05 +0100

    indentazione corretta

--- JavaScript/ES10.js
+++ JavaScript/ES10.js
@@ -1,12 +1,11 @@
-
 let jurassicParck = [["sorpione", "mosca"], "batterio", "tartaruga", [[[["dinosauro"]]]]]
 // console.log(jurassicParck.flat(4))
 
-let entries = ["marco", "simona",,,,,,"claudia"]
+let entries = ["marco", "simona", , , , , , "claudia"]
 
 // console.log(entries.flat())
 
-let jurassicParckChaos = jurassicParck.flatMap(creature => creature+ " 🦕")
+let jurassicParckChaos = jurassicParck.flatMap(creature => creature + " 🦕")
 
 
 // console.log(jurassicParckChaos)
@@ -18,7 +17,7 @@ const userEmail1 = "johnny@gmail.com      "
 // console.log(userEmail1.trimEnd())
 
 
-userProfiles = [["Mario", 51],["Kevin", 13],["Anna", 34]]
+userProfiles = [["Mario", 51], ["Kevin", 13], ["Anna", 34]]
 
 let userProfilesObject = Object.fromEntries(userProfiles) // converte un elenco di coppie chiave-valore in un nuovo oggetto le cui proprietà sono definite da tali coppie
 
--- JavaScript/debug_test.js
+++ JavaScript/debug_test.js
@@ -1,6 +1,6 @@
-
-
-const appiattimento = [[0,1], [2,3], [4,5]].reduce((accumulatore, array) =>{ debugger;
-    accumulatore.concat(array), []})
+const appiattimento = [[0, 1], [2, 3], [4, 5]].reduce((accumulatore, array) => {
+    debugger;
+    accumulatore.concat(array), []
+})
 
 console.log(appiattimento)
