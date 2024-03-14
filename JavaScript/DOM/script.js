0e02a77ffcda4e0cf04c590e9e2f7e80816694ef
diegosians <nuzzolo1000@gmail.com>
2024-03-14 22:09:24 +0100

    aggiornamento lista della spesa

--- JavaScript/DOM/script.js
+++ JavaScript/DOM/script.js
@@ -1,68 +1,81 @@
-var button = document.getElementById("enter");
-var editButton = document.getElementById("edit")
-var input = document.getElementById("userinput");
-var ul = document.querySelector("ul");
-var getItDoneToday = document.getElementById("first")
+let button = document.getElementById("enter");
+let editButton = document.getElementById("edit")
+let input = document.getElementById("userinput");
+let ul = document.querySelector("ul");
+let getItDoneToday = document.getElementById("first")
+let save = document.getElementById("save")
 
+function changeText() {
+	let id = document.getElementById("first")
+	if (id.textContent === "Per oggi fatto") { id.textContent = "Mostra ancora" }
+	else { id.textContent = "Per oggi fatto" }
+}
 
+function rmDelButton() {
 
-function rmDelButton(){
-	input.disabled = false
-	var delButton = document.querySelectorAll('.delButton')
+	let delButton = document.querySelectorAll('.btn.btn-danger')
 
-	for(var i=0; i<delButton.length; i++){
+	for (var i = 0; i < delButton.length; i++) {
 		delButton[i].remove()
 	}
 }
 
+function saveList() {
+	input.disabled = false
+	switchList()
+	rmDelButton()
+}
 
-var editList = function() {
+function switchList() {
+	if (document.getElementsByTagName("li")[0].children.length === 1) {
+		let allDelButton = document.querySelectorAll('.btn.btn-danger')
+		let li = document.getElementsByTagName("li")
 
-
-	input.disabled = true
-
-
-
-	if(document.getElementsByTagName("li")[0].children.length === 1){
-		var allDelButton = document.querySelectorAll(".delButton")
-		var li = document.getElementsByTagName("li")
-
-		for(var index =0; index<li.length; index++){
-			li[index].removeEventListener("click", allDelButton[index].remove)
-	}
+		for (let index = 0; index < li.length; index++) {
+			allDelButton[index].removeEventListener("click", allDelButton[index].remove)
+		}
 
 
 		rmDelButton()
 		return
 	}
+}
 
-	var li = document.getElementsByTagName("li")
+function editList() {
 
-	for(var i=0; i<li.length; i++){
 
-		var delButton = document.createElement("input")
+	input.disabled = true
+
+	switchList()
+
+
+
+	let li = document.getElementsByTagName("li")
+
+	for (let i = 0; i < li.length; i++) {
+
+		let delButton = document.createElement("input")
 		delButton.type = "button"
-		delButton.value = "X"
-		delButton.className = "delButton"
-		delButton.id ="delButton"+i
+		delButton.value = "-"
+		delButton.className = "btn btn-danger"
+		delButton.id = "delButton" + i
 		li[i].append(delButton)
 	}
 
-	var allDelButton = document.querySelectorAll(".delButton")
+	let allDelButton = document.querySelectorAll(".btn.btn-danger")
 
-	li[0].addEventListener("click", allDelButton[0].remove)
-	li[1].addEventListener("click", allDelButton[1].remove)
-	li[2].addEventListener("click", allDelButton[2].remove)
-	li[3].addEventListener("click", allDelButton[3].remove)
-	li[4].addEventListener("click", allDelButton[4].remove)
-	li[5].addEventListener("click", allDelButton[5].remove)
-	li[6].addEventListener("click", allDelButton[6].remove)
-	li[7].addEventListener("click", allDelButton[7].remove)
-	li[8].addEventListener("click", allDelButton[8].remove)
-	li[9].addEventListener("click", allDelButton[9].remove)
-	li[10].addEventListener("click", allDelButton[10].remove)
-
-
+	allDelButton[0].addEventListener("click", () => { allDelButton[0].parentElement.remove() })
+	allDelButton[1].addEventListener("click", () => { allDelButton[1].parentElement.remove() })
+	allDelButton[2].addEventListener("click", () => { allDelButton[2].parentElement.remove() })
+	allDelButton[3].addEventListener("click", () => { allDelButton[3].parentElement.remove() })
+	allDelButton[4].addEventListener("click", () => { allDelButton[4].parentElement.remove() })
+	allDelButton[5].addEventListener("click", () => { allDelButton[5].parentElement.remove() })
+	allDelButton[6].addEventListener("click", () => { allDelButton[6].parentElement.remove() })
+	allDelButton[7].addEventListener("click", () => { allDelButton[7].parentElement.remove() })
+	allDelButton[8].addEventListener("click", () => { allDelButton[8].parentElement.remove() })
+	allDelButton[9].addEventListener("click", () => { allDelButton[9].parentElement.remove() })
+	allDelButton[10].addEventListener("click", () => { allDelButton[10].parentElement.remove() })
+	allDelButton[11].addEventListener("click", () => { allDelButton[11].parentElement.remove() })
 
 
 }
@@ -77,9 +90,10 @@ function inputLength() {
 
 function createListElement() {
 	var liMax = document.getElementsByTagName("li")
-	if(liMax.length ===12){alert("Hai raggiunto il limite massimo della lista!"); return}
+	if (liMax.length === 12) { alert("Hai raggiunto il limite massimo della lista!"); return }
 	var li = document.createElement("li");
 	li.appendChild(document.createTextNode(input.value));
+	li.className = "list-group-item"
 	ul.appendChild(li);
 	input.value = "";
 }
@@ -99,11 +113,11 @@ function addListAfterKeypress(event) {
 function toOnOffUl() {
 
 
-    switch(ul.style.display){
-        case "none": ul.style.display = ""; editButton.style.display = ""; input.disabled = false
-            break
-        default: ul.style.display = "none"; editButton.style.display = "none"; input.disabled = true; rmDelButton()
-    }
+	switch (ul.style.display) {
+		case "none": ul.style.display = ""; editButton.disabled = false; input.disabled = false; save.disabled = false; button.disabled = false
+			break
+		default: ul.style.display = "none"; editButton.disabled = true; input.disabled = true; rmDelButton(); save.disabled = true; button.disabled = true
+	}
 
 }
 
