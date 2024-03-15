00b5694570f331ece164c73e6667f9726d5aaf6b
diegosians <nuzzolo1000@gmail.com>
2024-03-15 22:22:54 +0100

    OOP

--- JavaScript/Progr_Orientata_Oggetti/script.js
+++ JavaScript/Progr_Orientata_Oggetti/script.js
@@ -1,1 +1,23 @@
-console.log("testing")
+// in javascript abbiamo la possibilita di creare oggetti 
+// usando la funzione come costruttore 
+
+const Persona = function(nome, annoNascita){
+    this.nome = nome
+    this.annoNascita = annoNascita
+}
+
+const persona1 = new Persona("Gino", "1992")
+
+
+console.log(persona1 instanceof Persona)
+
+
+// Prototypes
+Persona.prototype.calcEta = function(){
+    console.log(2024- this.annoNascita)
+}
+
+persona1.calcEta()
+
+console.log(Persona.prototype.isPrototypeOf(persona1))
+
