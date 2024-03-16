4c60fa9293a8c5162d7dd7b73ef42d3062710242
diegosians <nuzzolo1000@gmail.com>
2024-03-16 17:57:37 +0100

    OOP: Object.create() learned

--- JavaScript/Progr_Orientata_Oggetti/Object.create/script.js
+++ JavaScript/Progr_Orientata_Oggetti/Object.create/script.js
@@ -1,14 +1,24 @@
 const PersonProto = {
     calcolaEta(){
         console.log(2024-this.annoNascita)
+    },
+    init(nome, annoNascita){
+        this.nome = nome
+        this.annoNascita = annoNascita
     }
 }
 
+// questo è un metodo per passare direttamente il prototype all'oggetto definito
 const p1 = Object.create(PersonProto)
-console.log(p1)
+// console.log(p1)
 
 p1.nome = "Giustino"
 
 p1.annoNascita = 2002
 
-p1.calcolaEta()
+// p1.calcolaEta()
+
+const p2 = Object.create(PersonProto)
+
+p2.init("Sarah", 2005)
+p2.calcolaEta()
