66c6367939ad2974589c2a5ef946fa2b4e8bd0ff
diegosians <nuzzolo1000@gmail.com>
2024-03-17 19:21:48 +0100

    aggiornamento file C#

--- C#/Console/esercizio1/obj/Debug/net8.0/esercizio1.AssemblyInfo.cs
+++ C#/Console/esercizio1/obj/Debug/net8.0/esercizio1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("esercizio1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d1fa1f925f0c4a4ed8023511160ce2929942110")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+107bbf48b39c578e318c05e9dd6ab7e00cd539d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("esercizio1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("esercizio1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
