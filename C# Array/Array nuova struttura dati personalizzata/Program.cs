466706902406492e89eb677e09f9b332fe037616
diegosians <nuzzolo1000@gmail.com>
2024-03-21 01:55:29 +0100

    upèdate

--- C# Array/Array nuova struttura dati personalizzata/Program.cs
+++ C# Array/Array nuova struttura dati personalizzata/Program.cs
@@ -8,16 +8,23 @@ internal class Program
 //  il parametro di valore bool permette di scegliere se il nostro oggetto
 //  deve avere lo stile dell'array o meno in fase di costruzione
         Vettore array = new Vettore(false);
-        
-        Object[] messaggio = {"My phone number:", 39, 3713936694};
+        Vettore array1 = new Vettore(true);
+
+        object[] messaggio = { "My phone number:", 39, 1234567890 };
         
         array.Aggiungi(messaggio);
 
         array.RimuoviUltimo();
 
-        array.Spingi(3394172276);
+        array.Spingi(3713936694);
 
         Console.WriteLine(array.ToString());
+        // accetta qualsiasi tipo di oggetto oppure solamente un array come parametro/i
+        array1.Aggiungi(true, 0.69812, 45, "Array", "con", "stile");
+        array1.Scarta();
+        array1.Scartane(2);
+        Console.WriteLine(array1.ToString());
+
 
     }
 }
