d2923bd38c86f7141bbf403a85b33389b8593ff5
diegosians <nuzzolo1000@gmail.com>
2024-03-21 11:48:42 +0100

    Array personalizzato Vettore update

--- C# Array/Array nuova struttura dati personalizzata/Program.cs
+++ C# Array/Array nuova struttura dati personalizzata/Program.cs
@@ -1,30 +1,51 @@
 ﻿// Array personalizzato
-namespace Vettore;
 
-internal class Program
+
+namespace Vettore
 {
-    private static void Main(string[] args)
+
+    internal class Program
     {
-//  il parametro di valore bool permette di scegliere se il nostro oggetto
-//  deve avere lo stile dell'array o meno in fase di costruzione
-        Vettore array = new Vettore(false);
-        Vettore array1 = new Vettore(true);
+        private static void Main(string[] args)
+        {
+            //  il parametro di valore bool permette di scegliere se il nostro oggetto
+            //  deve avere lo stile dell'array o meno in fase di costruzione
+            Vettore array = new Vettore(false);
+            Vettore array1 = new Vettore(true);
 
-        object[] messaggio = { "My phone number:", 39, 1234567890 };
-        
-        array.Aggiungi(messaggio);
+            object[] messaggio = { "My phone number:", 39, 1234567890 };
+            // inizio setting manuale
+            array.Aggiungi(messaggio);
 
-        array.RimuoviUltimo();
+            array.RimuoviUltimo();
 
-        array.Spingi(3713936694);
+            array.Spingi(3713936694);
+            // fine setting manuale
 
-        Console.WriteLine(array.ToString());
-        // accetta qualsiasi tipo di oggetto oppure solamente un array come parametro/i
-        array1.Aggiungi(true, 0.69812, 45, "Array", "con", "stile");
-        array1.Scarta();
-        array1.Scartane(2);
-        Console.WriteLine(array1.ToString());
+            // inizio setting rapido
+            array = new Vettore(false)
+            {
+                // oppure nel caso di un Vettore inizializzato in precedenza, si puo' usare array.Data = messaggio; o array.Data = ["My phone number:", 39, 1234567890]
+                Data = messaggio
+            };
+            // fine setting rapido
 
+            Console.WriteLine(array.ToString());
+            // accetta qualsiasi tipo di oggetto oppure solamente un array come parametro/i
+            array1.Aggiungi(true, 0.69812, 45, "Array", "con", "stile");
+            array1.Scarta();
+            array1.Scartane(2);
+            // Console.WriteLine(array1.ToString()); output --> ["Array", "con", "stile"]
 
+            // semplice modo per il reset di un oggetto array
+            array1.Data = ["Array", "con", "stile"];
+            array1.con_stile = true;
+            Console.WriteLine(array1.ToString());
+
+            // Vettore per assegnazione in dichiarazione
+            Vettore vet = new Vettore(false) { Data = ["Ultimo", "oggetto di array", "dimostrativo"] };
+            Console.Write(vet.ToString());
+
+        }
     }
 }
--- C# Array/Array nuova struttura dati personalizzata/Vettore.cs
+++ C# Array/Array nuova struttura dati personalizzata/Vettore.cs
@@ -1,130 +1,135 @@
-// Array personalizzato
-
-
 namespace Vettore
 {
-    class Vettore
+public class Vettore
+{
+    public int Length;
+    private Object[] data;
+    public bool con_stile;
+    public Vettore(bool con_o_senza_stile)
     {
-        public int Length;
-        private Object[] data;
-        public bool con_stile;
-        public Vettore(bool con_o_senza_stile)             // Qui è il costruttore
+        this.data = new Object[0];
+        Length = this.data.Length;
+        con_stile = con_o_senza_stile;
+    }
+    public Object Get(int indice_elemento)
+    {
+        return this.data[indice_elemento];
+    }
+
+    public Object[] Data
+    {
+        get { return data; }
+        set { data = value; Length = data.Length; }
+    }
+
+
+    public void Spingi(Object obj)
+    {
+        Length++;
+        Object[] data;
+        data = this.data;
+        this.data = new Object[Length];
+        for (int i = 0; i < data.Length; i++)
         {
-            this.data = new Object[0];
-            Length = this.data.Length;
-            con_stile = con_o_senza_stile;
+            this.data[i] = data[i];
         }
-        public Object Get(int indice_elemento){
-            return this.data[indice_elemento];
-        }
-        public void Spingi(Object obj)
+        this.data[Length - 1] = obj;
+    }
+    public void Aggiungi(params Object[] obj)
+    {
+        Length += obj.Length;
+        Object[] data = this.data;
+        this.data = new Object[Length];
+        for (int i = 0; i < obj.Length; i++) { this.data[i] = obj[i]; }
+        for (int i = obj.Length; i < Length; i++) { this.data[i] = data[i - obj.Length]; }
+    }
+    public void Aggiungi<Object>(Object[] arg)
+    {
+        Length += arg.Length;
+        object[] data = this.data;
+        this.data = new object[Length];
+        for (int i = 0; i < arg.Length; i++)
         {
-            Length++;
-            Object[] data;
-            data = this.data;
-            this.data = new Object[Length];
-            for (int i = 0; i < data.Length; i++)
-            {
-                this.data[i] = data[i];
-            }
-            this.data[Length - 1] = obj;
-        }
-        public void Aggiungi(params Object[] obj)
-        {
-            Length += obj.Length;
-            Object[] data = this.data;
-            this.data = new Object[Length];
-            for(int i=0; i<obj.Length; i++){this.data[i] = obj[i];}
-            for (int i =obj.Length; i<Length; i++){this.data[i] = data[i-obj.Length];}
-        }
-        public void Aggiungi<Object>(Object[] arg)
-        {
-            Length += arg.Length;
-            object[] data = this.data;
-            this.data = new object[Length];
-//          assegnazione dei molteplici oggetti all'array 
-            for(int i = 0; i<arg.Length; i++)
-            {
-#pragma warning disable CS8601 // Possibile assegnazione di riferimento Null.
-                this.data[i] = arg[i];
-#pragma warning restore CS8601 // Possibile assegnazione di riferimento Null.
-            }
-            
-            for (int i = arg.Length; i < Length; i++)
-            {
-                this.data[i] = data[i - arg.Length];
-            }
+#pragma warning disable CS8601 
+            this.data[i] = arg[i];
+#pragma warning restore CS8601 
         }
 
-
-        public void RimuoviUltimo()
+        for (int i = arg.Length; i < Length; i++)
         {
-            Length--;
-            Object[] data = this.data;
-            this.data = new Object[Length];
-            for (int i=0; i<Length; i++)
-            {
-                this.data[i] = data[i];
-            }
-            
-        }
-
-        // questa funzione rimuove il primo elemento dell'array
-        public void Scartane(int indice)
-        {
-            if(indice!=0)
-            {
-                Length -= indice;
-                Object[] data = this.data;
-                this.data = new Object[Length];
-                for (int i = indice; i < data.Length; i++)
-                {
-                    this.data[i-indice] = data[i];
-                }      
-            }
-            else{ Scarta(); }
-
-        }
-
-        public void Scarta()
-        {
-            Length--;
-            Object[] data = this.data;
-            this.data = new Object[Length];
-            for (int i = 1; i < data.Length; i++)
-            {
-                this.data[i - 1] = data[i];
-            }
-        }
-
-
-
-        public override String ToString()
-        {
-            String text = "";
-
-            if (con_stile)
-            {
-                text = "[";
-                for (int i = 0; i < Length; i++)
-                {
-                    text += this.data[i];
-                    if (i == Length - 1) break;
-                    text += ", ";
-
-                }
-                text += "]";
-            }
-            else
-            {
-                for (int i = 0; i < Length; i++)
-                {
-                    text += this.data[i];
-                    if (i == Length - 1) break;
-                    text += " ";
-                }
-            }
-            return text;
+            this.data[i] = data[i - arg.Length];
         }
     }
+
+
+    public void RimuoviUltimo()
+    {
+        Length--;
+        Object[] data = this.data;
+        this.data = new Object[Length];
+        for (int i = 0; i < Length; i++)
+        {
+            this.data[i] = data[i];
+        }
+
+    }
+
+
+    public void Scartane(int indice)
+    {
+        if (indice != 0)
+        {
+            Length -= indice;
+            Object[] data = this.data;
+            this.data = new Object[Length];
+            for (int i = indice; i < data.Length; i++)
+            {
+                this.data[i - indice] = data[i];
+            }
+        }
+        else { Scarta(); }
+
+    }
+
+    public void Scarta()
+    {
+        Length--;
+        Object[] data = this.data;
+        this.data = new Object[Length];
+        for (int i = 1; i < data.Length; i++)
+        {
+            this.data[i - 1] = data[i];
+        }
+    }
+
+
+
+    public override String ToString()
+    {
+        String text = "";
+
+        if (con_stile)
+        {
+            text = "[";
+            for (int i = 0; i < Length; i++)
+            {
+                text += this.data[i];
+                if (i == Length - 1) break;
+                text += ", ";
+
+            }
+            text += "]";
+        }
+        else
+        {
+            for (int i = 0; i < Length; i++)
+            {
+                text += this.data[i];
+                if (i == Length - 1) break;
+                text += " ";
+            }
+        }
+        return text;
+    }
+}
 }
